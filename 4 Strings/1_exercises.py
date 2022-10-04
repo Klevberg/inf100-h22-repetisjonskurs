4e4c69e072657a99a64811d3eea68f35bcaa754c
Klevberg <augustkl@hotmail.com>
2022-10-04 20:04:27 +0200

    Added more details to the string exercises

--- 4 Strings/1_exercises.py
+++ 4 Strings/1_exercises.py
@@ -1,26 +1,39 @@
-def char_count(s):
-    """
-    Print the count of each character in the given string
-    """
+"""
+Given a string, print each character and its corresponding index
+Example:
+char_count("Hello") should print:
+Index 0: H
+Index 1: e
+Index 2: l
+Index 3: l
+Index 4: o
+"""
+def print_index_char(string):
     pass
 
+# print_index_char("Hello")
 
-def reverse_string(s):
-    """
-    Print the given word reversed
-    """
+
+"""
+Return the given string reversed
+Example:
+reverse_string("hello") should return "olleh"
+"""
+def reverse_string(string):
     pass
 
+# print(reverse_string("Hello"))
 
-def mix(s1, s2):
-    """
-    count the occurrences of each word in a given sentence
-    """
+
+"""
+Given a string, return a string that has its final half moved to the front.
+If the string is odd in length, the middle element also gets moved.
+Examples:
+swap_string("Hello, world") returns " worldHello,"
+swap_string("Hello") returns "lloHe"
+"""
+def swap_string(string):
     pass
 
-
-def swap_chars(s):
-    """
-    change a given string to a new string where the first and last chars have been exchanged.
-    """
-    pass
+# print(swap_string("Hello, world"))
+# print(swap_string("Hello"))
