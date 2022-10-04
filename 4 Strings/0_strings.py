1a36eca711d56c22d53ec14849827347b059bc70
Klevberg <augustkl@hotmail.com>
2022-10-04 19:48:39 +0200

    Translated from Norwegian to English

--- 4 Strings/0_strings.py
+++ 4 Strings/0_strings.py
@@ -1,114 +1,96 @@
-def eks_1():
-    # I kildekoden kan streng-verdier oppgis på fire ulike måter
-    print('apostrof')
-    print("hermetegn")
-    print('''trippel-apostrof''')
-    print("""trippel-hermetegn""")
+# We can use either double or single quotes to represent strings
+print('foo' == "foo")  # True
+print()
 
-    # Hvilken variant som brukes har absolutt ingenting å si
-    print('foo' == "foo")  # True
+# This is how we use quotes in our strings
+print("Let's use 'single quotes'")
+print('Let\'s use "double quotes"')
+print()
 
-    # Så hvorfor ha flere varianter?
-    # Svar 1: kompabilititet
-    # Svar 2: for å enklere skrive hermetegn og apostrof
-    print("Her er 'apostrof'")
-    print('Her er "hermetegn"')
-    print("""Her er både "hermetegn" og 'apostrofer'""")
+print("abc" + "def")    # concatenation
+print("abc" * 3)        # repetition
+print(len("abc"))       # length
+print()
+
+# The backslash can be used to 'escape' a character
+print("Newline: [\n]")  # becomes a newline
+print("Tab: [\t]")      # becomes a tab
+print("Backslash: \\")  # becomes a backslash
+print()
+
+# An escaped character counts as a single character
+string = "a\\b\"c\td"
+print("string =", string)
+print("len(string) =", len(string))
 
 
-def eks_2():
-    print("Hermetegn i hermetegn-streng: \"")
-    print("Bakstrek: \\")
-    print("Linjeskift: [\n]")
-    print("Tab: [\t]")
+def print_string_conversion(x):
+    print("type:", type(x)) # shows the type of a variable x
+    print(" str:", str(x))  # shows the string as represented in the terminal
+    print("repr:", repr(x)) # shows the string as stored in the variable x
     print()
 
-    print("Denne teksten er skilt av tab'er, 3 per linje:")
-    print("abc\tdef\tg\nhi\tj\\\tk\n---")
-    print()
+print_string_conversion(10)
+print_string_conversion(True)
+print_string_conversion(2 / 11)
 
-    # En escape-sekvens telles som ett tegn
-    s = "a\\b\"c\td"
-    print("s =", s)
-    print("len(s) =", len(s))
+print_string_conversion("Mellomrom\ttab")
+print_string_conversion("Linje\nskift  ")
 
 
-def eks_3():
-    def print_string_conversion(x):
-        print("type:", type(x))
-        print(" str:", str(x))
-        print("repr:", repr(x))
-        print()
+s = "abcdef"
 
-    print("Vanligvis konverteres verdier til streng med str-funksjonen")
-    print("Å bruke repr-funksjonen gir for mange vanlige typer samme resultat")
-    print_string_conversion(10)
-    print_string_conversion(True)
-    print_string_conversion(2 / 11)
+# Indexing of strings
+print(s[0])
+print(s[-1])
 
-    print("Men for strenger, viser repr oss whitespace og escape-sekvenser.")
-    print_string_conversion("   Mellomrom\ttab   ")
-    print_string_conversion("Linje\nskift")
-
-    # Generelt vil `repr` vise mer detaljert informasjon enn `str`.
-    # Hensikten med `str` er at resultatet skal være leselig, hensikten
-    # med `repr` er å gi oss presis informasjon.
-
-    print("For andre typer kan forskjellen være stor")
-    import datetime
-    today = datetime.datetime.now()
-    print_string_conversion(today)
+# Slicing of strings
+print(s[1:4:2])
 
 
-def eks_4():
-    s = "abcdef"
-    print(s[0])
-    print(s[-1])
-    print(s[1:4:2])
+# Membership
+print("a" in "abc")
+print("bc" in "abc")
+print("ca" in "abc")
 
 
-def eks_5():
-    print("abc" + "def")  # Konkatenasjon
-    print("abc" * 3)  # Repetisjon
-    print(len("abc"))  # Lengde
-    print()
-
-    # Medlemskap
-    print('"a" in "abc"')
-    print("a" in "abc")
-    print('"bc" in "abc"')
-    print("bc" in "abc")
-    print('"ca" in "abc"')
-    print("ca" in "abc")
-    print('"A" in "abc"')
-    print("A" in "abc")
-    print('"" in "abc"')
-    print("" in "abc")
-    
-
-def eks_6():
-    a = "abcdef"
-    print(a)
-    print(a.capitalize())
-    print(a.upper())
-    
-    print(f"is a upper")
+# Loop through each character in a string
+for c in s:
+    print(c)
 
 
-def eks_7():
-    a = "abcdef"
-    print(a)
-    print("for i in range(len(a))")
-    for i in range(len(a)):
-        print(a[i])
-    print("for c in a")
-    for c in a:
-        print(c)
+# Loop through a string using indexing
+for i in range(len(s)):
+    print(i, s[i])
+    # s[i] = "x" # Crash
+print()
 
 
-def eks_8():
-    a = "abcdef"
-    print(a)
-    for i in range(len(a)):
-        a[i] = "a"
-    print(a)
+# String methods: https://www.w3schools.com/python/python_strings_methods.asp
+print(s)
+print()
+
+print(s.capitalize())
+print(s)
+print()
+
+s = s.upper()
+print(s)
+print()
+
+s = s.lower()
+print(s)
+print()
+
+print(s.count('b'))
+print(s.index('c'))
+print()
+
+s = ' hello    \n'
+print(s)
+print(len(s))
+print()
+
+s = s.strip()
+print(s)
+print(len(s))
